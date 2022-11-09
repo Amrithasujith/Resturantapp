5f6b4359dcb7d55fde6879857d4ece03c61316ae
amrithas <amrithas@opentrends.net>
2022-11-09 12:53:19 +0530

    changed the data

--- src/Stocks.js
+++ src/Stocks.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { Image, Text, View } from "react-native";
 import "./App.css";
@@ -7,45 +9,56 @@ import { dairy } from "./data";
 
 import { meat } from "./data";
 
-
 export const Stocks = () => {
   return (
     <>
-    <View style={{
-            padding: 10,
-          }}>
-     <Text
+      <View
+        style={{
+          padding: 10,
+        }}
+      >
+        <Text
           style={{
             paddingTop: 30,
-            color:'#30384d',
+            color: "#30384d",
             fontFamily: "Montserrat",
             fontWeight: "bold",
             fontSize: 20,
-            
           }}
         >
           Vegetables
         </Text>
-      <div style={{display:"flex"}}>
-       
-        {vegetableData.map((data, key) => {
-          return (
-            
+        <div style={{ display: "flex" }}>
+          {vegetableData.map((data, key) => {
+            return (
               <span key={{ key }}>
                 <Image
-                  style={{ width: 100, height: 100, margin: 5, borderRadius:5}}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    margin: 5,
+                    borderRadius: 5,
+                  }}
                   source={{ uri: data.imageUrl }}
                 />
-                <div style={{ padding : 5, color: '#7e8a9a', fontFamily:'Montserrat', fontSize:13 }}>{data.Ingredient}</div>
+                <div
+                  style={{
+                    padding: 5,
+                    color: "#7e8a9a",
+                    fontFamily: "Montserrat",
+                    fontSize: 13,
+                  }}
+                >
+                  {data.Ingredient}
+                </div>
               </span>
-            
-          );
-        })}
-      </div>
-      <Text
+            );
+          })}
+        </div>
+        <Text
           style={{
             paddingTop: 30,
-            
+
             fontFamily: "Montserrat",
             fontWeight: "bold",
             fontSize: 20,
@@ -53,27 +66,38 @@ export const Stocks = () => {
         >
           Fruits
         </Text>
-      <div style={{display:"flex"}}>
-       
-        {fruitData.map((data, key) => {
-          return (
-            
+        <div style={{ display: "flex" }}>
+          {fruitData.map((data, key) => {
+            return (
               <span key={{ key }}>
                 <Image
-                  style={{ width: 100, height: 100, margin: 5, borderRadius:5 }}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    margin: 5,
+                    borderRadius: 5,
+                  }}
                   source={{ uri: data.imageUrl }}
                 />
-                <div style={{ padding : 5, color: '#7e8a9a', fontFamily:'Montserrat', fontSize:13 }}>{data.Ingredient}</div>
+                <div
+                  style={{
+                    padding: 5,
+                    color: "#7e8a9a",
+                    fontFamily: "Montserrat",
+                    fontSize: 13,
+                  }}
+                >
+                  {data.Ingredient}
+                </div>
               </span>
-            
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
 
-      <Text
+        <Text
           style={{
             paddingTop: 30,
-            
+
             fontFamily: "Montserrat",
             fontWeight: "bold",
             fontSize: 20,
@@ -81,52 +105,72 @@ export const Stocks = () => {
         >
           Dairy
         </Text>
-      <div style={{display:"flex"}}>
-       
-        {dairy.map((data, key) => {
-          return (
-            
+        <div style={{ display: "flex" }}>
+          {dairy.map((data, key) => {
+            return (
               <span key={{ key }}>
                 <Image
-                  style={{ width: 100, height: 100, margin: 5, borderRadius:5 }}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    margin: 5,
+                    borderRadius: 5,
+                  }}
                   source={{ uri: data.imageUrl }}
                 />
-                <div style={{ padding : 5, color: '#7e8a9a', fontFamily:'Montserrat', fontSize:13 }}>{data.Ingredient}</div>
+                <div
+                  style={{
+                    padding: 5,
+                    color: "#7e8a9a",
+                    fontFamily: "Montserrat",
+                    fontSize: 13,
+                  }}
+                >
+                  {data.Ingredient}
+                </div>
               </span>
-            
-          );
-        })}
-      </div>
-      <Text
+            );
+          })}
+        </div>
+        <Text
           style={{
             paddingTop: 30,
-            
+
             fontFamily: "Montserrat",
             fontWeight: "bold",
             fontSize: 20,
           }}
         >
-          Meat      </Text>
-      <div style={{display:"flex"}}>
-       
-        {meat.map((data, key) => {
-          return (
-            
+          Meat{" "}
+        </Text>
+        <div style={{ display: "flex" }}>
+          {meat.map((data, key) => {
+            return (
               <span key={{ key }}>
                 <Image
-                  style={{ width: 100, height: 100, margin: 5, borderRadius:5 }}
+                  style={{
+                    width: 100,
+                    height: 100,
+                    margin: 5,
+                    borderRadius: 5,
+                  }}
                   source={{ uri: data.imageUrl }}
                 />
-                <div style={{ padding : 5, color: '#7e8a9a', fontFamily:'Montserrat', fontSize:13 }}>{data.Ingredient}</div>
+                <div
+                  style={{
+                    padding: 5,
+                    color: "#7e8a9a",
+                    fontFamily: "Montserrat",
+                    fontSize: 13,
+                  }}
+                >
+                  {data.Ingredient}
+                </div>
               </span>
-            
-          );
-        })}
-      </div>
-      
+            );
+          })}
+        </div>
       </View>
-      
-
     </>
   );
 };
